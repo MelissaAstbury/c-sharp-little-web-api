b63fb0c5f724a3eac321427c23896912095bba5f
Melissa Astbury <melissaastbury@hotmail.com>
2021-03-29 20:05:12 +0100

    Added a new project for testing. I have tested that the length of the Elephants when GetElephants is called is the correct amount expected

---
+++ ElephantsProjectTests/ElephantsTests.cs
@@ -0,0 +1,27 @@
+using System;
+using ElephantsProject;
+using Xunit;
+
+namespace ElephantsTests
+{
+    public class ElephantsProjectTests
+    {
+        internal ElephantService _tests = new ElephantService();
+    }
+
+    public class ElephantTests : ElephantsProjectTests
+    {
+        [Fact]
+        public void LoadsAllElephantsWithExpectedValues()
+        {
+            // arrange 
+            var totalElephantsLength = 25;
+
+            // act
+            var result = _tests.GetElephants();
+
+            // assert
+            Assert.Equal(totalElephantsLength, result.Count);
+        }
+    }
+}
